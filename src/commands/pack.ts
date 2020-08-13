e50cd6d10c0df84bd031b3d8e320960fac5e3ee6
shuyaqian <717749594@qq.com>
2020-08-13 16:29:05 +0800

    add code comments and eslint config

--- src/commands/pack.ts
+++ src/commands/pack.ts
@@ -7,84 +7,100 @@ import * as path from "path";
 import * as fs from "fs";
 import * as readPkg from "read-pkg";
 import { Packing } from "../common/packing";
-import { PackType, CheckType, SpecialFiles } from '../common/pack-configuration';
-import { fileMatch } from '../common/file-matcher';
+import {
+  PackType,
+  CheckType,
+  SpecialFiles,
+} from "../common/pack-configuration";
+import { fileMatch } from "../common/file-matcher";
 
 const pluginRootFolder = path.resolve(process.cwd());
-const specialFiles: SpecialFiles = { 'include': [], 'exclude': [] };
-const defaultExcludeFolders = ['node_modules', '.git'];
+const specialFiles: SpecialFiles = { include: [], exclude: [] };
+const defaultExcludeFolders = ["node_modules", ".git"];
 
-export async function pack(type: PackType, excludeFiles: string[], includeFiles: string[], skipPrepare: boolean) {
-    const moduleName = (await readPkg()).name;
-    if (moduleName.indexOf('.') !== -1) {
-        console.warn('⚠️ Package name can not include "." character.');
-        return;
-    }
-    const allFiles = await getAllFiles(pluginRootFolder);
-    if (excludeFiles.length) {
-        await checkRules(excludeFiles);
-        await getMatchFiles('exclude', excludeFiles, allFiles);
-    }
-    if (includeFiles.length) {
-        await checkRules(includeFiles);
-        await getMatchFiles('include', includeFiles, allFiles);
-    }
-    const { exclude, include } = specialFiles;
-    if (type === 'production') {
-        new Packing('production', exclude, include, allFiles).start(skipPrepare);
-    } else {
-        new Packing('development', exclude, include, allFiles).start(skipPrepare);
-    }
+/**
+ * Packing pre-operations to filter out files to be excluded and included.
+ * @param type pack type: production/development
+ * @param excludeFiles files array to exclude
+ * @param includeFiles files array to include
+ * @param skipPrepare skip npm install
+ */
+export async function pack(
+  type: PackType,
+  excludeFiles: string[],
+  includeFiles: string[],
+  skipPrepare: boolean
+) {
+  const moduleName = (await readPkg()).name;
+  if (moduleName.indexOf(".") !== -1) {
+    console.warn('⚠️ Package name can not include "." character.');
+    return;
+  }
+  const allFiles = await getAllFiles(pluginRootFolder);
+  if (excludeFiles.length) {
+    await checkRules(excludeFiles);
+    await getMatchFiles("exclude", excludeFiles, allFiles);
+  }
+  if (includeFiles.length) {
+    await checkRules(includeFiles);
+    await getMatchFiles("include", includeFiles, allFiles);
+  }
+  const { exclude, include } = specialFiles;
+  if (type === "production") {
+    new Packing("production", exclude, include, allFiles).start(skipPrepare);
+  } else {
+    new Packing("development", exclude, include, allFiles).start(skipPrepare);
+  }
 }
 
 function checkRules(rules: string[]) {
-    rules.forEach((rule) => {
-        defaultExcludeFolders.forEach((item) => {
-            if (rule.indexOf(item) !== -1) {
-                console.warn(`⚠️ Can not change the packaging mode of ${item}.`);
-            }
-        });
+  rules.forEach((rule) => {
+    defaultExcludeFolders.forEach((item) => {
+      if (rule.indexOf(item) !== -1) {
+        console.warn(`⚠️ Can not change the packaging mode of ${item}.`);
+      }
     });
+  });
 }
 
 function getMatchFiles(type: CheckType, matchRules: string[], files: string[]) {
-    let matchFiles: string[] = [];
-    let temps = [...Array(matchRules.length).fill(0)];
-    files.forEach((file) => {
-        matchRules.forEach((rule, index) => {
-            let filter = fileMatch(rule);
-            if (filter(file)) {
-                ++temps[index];
-                matchFiles.push(file);
-            }
-        });
+  const matchFiles: string[] = [];
+  const temps = [...Array(matchRules.length).fill(0)];
+  files.forEach((file) => {
+    matchRules.forEach((rule, index) => {
+      const filter = fileMatch(rule);
+      if (filter(file)) {
+        ++temps[index];
+        matchFiles.push(file);
+      }
     });
-    temps.forEach((temp, index) => {
-        if (!temp && defaultExcludeFolders.indexOf(matchRules[index]) === -1) {
-            console.warn(`⚠️ "${matchRules[index]}" does not math any files.`)
-        }
-    })
-    specialFiles[type] = matchFiles;
-    return matchFiles;
+  });
+  temps.forEach((temp, index) => {
+    if (!temp && defaultExcludeFolders.indexOf(matchRules[index]) === -1) {
+      console.warn(`⚠️ "${matchRules[index]}" does not math any files.`);
+    }
+  });
+  specialFiles[type] = matchFiles;
+  return matchFiles;
 }
 
 function getAllFiles(folder: string) {
-    let allFiles: string[] = [];
-    function findFile(folder: string) {
-        let files = fs.readdirSync(folder);
-        files.forEach((item, index) => {
-            let fPath = path.join(folder, item);
-            let stat = fs.statSync(fPath);
-            if (defaultExcludeFolders.indexOf(item) !== -1) {
-                return;
-            }
-            if (stat.isFile() === true) {
-                allFiles.push(path.relative(pluginRootFolder, fPath));
-            } else {
-                findFile(fPath);
-            }
-        });
-    }
-    findFile(folder);
-    return allFiles;
+  const allFiles: string[] = [];
+  function findFile(folder: string) {
+    const files = fs.readdirSync(folder);
+    files.forEach((item, index) => {
+      const fPath = path.join(folder, item);
+      const stat = fs.statSync(fPath);
+      if (defaultExcludeFolders.indexOf(item) !== -1) {
+        return;
+      }
+      if (stat.isFile() === true) {
+        allFiles.push(path.relative(pluginRootFolder, fPath));
+      } else {
+        findFile(fPath);
+      }
+    });
+  }
+  findFile(folder);
+  return allFiles;
 }
--- src/common/file-matcher.ts
+++ src/common/file-matcher.ts
@@ -3,48 +3,52 @@
  * SPDX-License-Identifier: MIT
  ********************************************************************************/
 
+/**
+ * Return files based on the file matching rule.
+ * @param filter filter rule
+ */
 export function fileMatch(filter: string) {
-    let match: string[] = [];
-    let negate: string[] = [];
-    const isNegate = filter.indexOf('!') === 0;
+  const match: string[] = [];
+  const negate: string[] = [];
+  const isNegate = filter.indexOf("!") === 0;
 
-    filter = filter
-        .replace(/^!/, '')
-        .replace(/\*(?![\/*])/, '[^/]*?')
-        .replace('**\/', '([^/]+\/)*')
-        .replace(/\{([^\}]+)\}/g, ($1, $2) => {
-            const collection = $2.split(',');
-            let result = '(?:';
+  filter = filter
+    .replace(/^!/, "")
+    .replace(/\*(?![\/*])/, "[^/]*?")
+    .replace("**/", "([^/]+/)*")
+    .replace(/\{([^\}]+)\}/g, ($1, $2) => {
+      const collection = $2.split(",");
+      let result = "(?:";
 
-            collection.forEach((item: string, index: number) => {
-                result += '(' + item.trim() + ')';
-                if (index + 1 !== collection.length) {
-                    result += '|';
-                }
-            });
-            result += ')';
-            return result;
-        })
-        .replace(/([\/\.])/g, '\\$1');
+      collection.forEach((item: string, index: number) => {
+        result += "(" + item.trim() + ")";
+        if (index + 1 !== collection.length) {
+          result += "|";
+        }
+      });
+      result += ")";
+      return result;
+    })
+    .replace(/([\/\.])/g, "\\$1");
 
-    filter = '(^' + filter + '$)';
+  filter = "(^" + filter + "$)";
 
-    if (isNegate) {
-        negate.push(filter);
-    } else {
-        match.push(filter);
+  if (isNegate) {
+    negate.push(filter);
+  } else {
+    match.push(filter);
+  }
+
+  const matchRule = match.length ? new RegExp(match.join("|")) : null;
+  const negateRule = negate.length ? new RegExp(negate.join("|")) : null;
+
+  return (filepath: string) => {
+    if (negateRule) {
+      return negateRule.test(filepath) ? false : true;
     }
-
-    const matchRule = match.length ? new RegExp(match.join('|')) : null;
-    const negateRule = negate.length ? new RegExp(negate.join('|')) : null;
-
-    return (filepath: string) => {
-        if (negateRule) {
-            return negateRule.test(filepath) ? false : true;
-        }
-        if (matchRule) {
-            return matchRule.test(filepath) ? true : false;
-        }
-        return false;
-    };
+    if (matchRule) {
+      return matchRule.test(filepath) ? true : false;
+    }
+    return false;
+  };
 }
