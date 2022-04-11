5f2d8d025a95c47ce0bd0132175a3e5a8d2d6d16
shuyaqian <717749594@qq.com>
2022-04-11 09:48:59 +0800

    format code with prettierrc

--- src/common/dependencies.ts
+++ src/common/dependencies.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: MIT
  ********************************************************************************/
 
-const { spawn } = require("child_process");
+import { spawn } from 'child_process';
 
 /**
  * Run `npm i` and `npm list --parseable--prod` to get dependencies.
@@ -11,55 +11,53 @@ const { spawn } = require("child_process");
  * @param skipPrepare skip npm install or not
  */
 export function getDependencies(skipPrepare: boolean): Promise<string[]> {
-  if (skipPrepare) {
-    console.log("Excuting packing...");
-    return getInstalledPkgs();
-  }
-  console.log("Excuting npm install...");
-  return new Promise<string[]>((resolve, reject) => {
-    const install = spawn("npm", ["i"], { stdio: "inherit", shell: true });
-    install.on("close", (code: number) => {
-      if (code !== 0) {
-        console.error(
-          "❗️  Npm install failed, check your dependencies in package.json please."
-        );
-        reject(new Error(`Npm install process exited with code ${code}`));
-      } else {
-        getInstalledPkgs()
-          .then((result) => {
-            resolve(result);
-          })
-          .catch((err) => {
-            reject(err);
-          });
-      }
+    if (skipPrepare) {
+        console.log('Excuting packing...');
+        return getInstalledPkgs();
+    }
+    console.log('Excuting npm install...');
+    return new Promise<string[]>((resolve, reject) => {
+        const install = spawn('npm', ['i'], { stdio: 'inherit', shell: true });
+        install.on('close', (code: number) => {
+            if (code !== 0) {
+                console.error('❗️  Npm install failed, check your dependencies in package.json please.');
+                reject(new Error(`Npm install process exited with code ${code}`));
+            } else {
+                getInstalledPkgs()
+                    .then((result) => {
+                        resolve(result);
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    });
+            }
+        });
     });
-  });
 }
 
 function getInstalledPkgs(): Promise<string[]> {
-  return new Promise<string[]>((resolve, reject) => {
-    const list = spawn("npm", ["list", "--parseable", "--prod"], { shell: true });
-    let dependencies: string[] = [];
-    list.stdout.on("data", (data: Buffer) => {
-      dependencies = data.toString().split("\n");
+    return new Promise<string[]>((resolve, reject) => {
+        const list = spawn('npm', ['list', '--parseable', '--prod'], { shell: true });
+        let dependencies: string[] = [];
+        list.stdout.on('data', (data: Buffer) => {
+            dependencies = data.toString().split('\n');
+        });
+        list.stderr.on('data', (err: Buffer) => {
+            console.error(`${err}`);
+        });
+        list.on('close', (code: number) => {
+            if (code !== 0) {
+                console.error(
+                    '❗️  Run npm list failed, please delete node_modules and run npm install, then try again.'
+                );
+                reject(new Error(`Npm list process exited with code ${code}`));
+            } else {
+                resolve(
+                    dependencies.filter((value) => {
+                        return value.indexOf('node_modules') !== -1;
+                    })
+                );
+            }
+        });
     });
-    list.stderr.on("data", (err: Buffer) => {
-      console.error(`${err}`);
-    });
-    list.on("close", (code: number) => {
-      if (code !== 0) {
-        console.error(
-          "❗️  Run npm list failed, please delete node_modules and run npm install, then try again."
-        );
-        reject(new Error(`Npm list process exited with code ${code}`));
-      } else {
-        resolve(
-          dependencies.filter((value) => {
-            return value.indexOf("node_modules") !== -1;
-          })
-        );
-      }
-    });
-  });
 }
--- tests/achiver.test.ts
+++ tests/achiver.test.ts
@@ -1,53 +1,56 @@
-import * as fs from "fs";
-import * as path from "path";
-import { zip } from "../src/common/archiver";
-import * as unzipper from "unzipper";
+import * as fs from 'fs';
+import * as path from 'path';
+import { zip } from '../src/common/archiver';
+import * as unzipper from 'unzipper';
 import { expect } from 'chai';
 
-describe("test zip function", () => {
+describe('test zip function', () => {
     const rootFolder = process.cwd();
-    const zipExamplePath = path.resolve(rootFolder, "tests/mock");
-    const zipPath = path.resolve(zipExamplePath, "mock.zip");
+    const zipExamplePath = path.resolve(rootFolder, 'tests/mock');
+    const zipPath = path.resolve(zipExamplePath, 'mock.zip');
 
     afterEach(() => {
         fs.unlinkSync(zipPath);
     });
 
-    it("test zip file", async () => {
-        await zip([path.resolve(zipExamplePath, "demo.entry"), path.resolve(zipExamplePath, "folder/sub.entry")],
-            zipPath, zipExamplePath);
+    it('test zip file', async () => {
+        await zip(
+            [path.resolve(zipExamplePath, 'demo.entry'), path.resolve(zipExamplePath, 'folder/sub.entry')],
+            zipPath,
+            zipExamplePath
+        );
 
         let findEntry = false;
         let findSubentry = false;
         let findOtherEntry = null;
 
-        await fs.createReadStream(zipPath)
+        await fs
+            .createReadStream(zipPath)
             .pipe(unzipper.Parse())
-            .on("entry", (entry) => {
+            .on('entry', (entry) => {
                 const fileName = entry.path;
-                const type = entry.type;
-                const size = entry.size;
-                if (fileName === "demo.entry") {
+                if (fileName === 'demo.entry') {
                     findEntry = true;
-                } else if (fileName === "folder/sub.entry") {
+                } else if (fileName === 'folder/sub.entry') {
                     findSubentry = true;
                 } else {
                     findOtherEntry = fileName;
                 }
                 entry.autodrain();
-            }).promise();
+            })
+            .promise();
 
         expect(findEntry).to.be.true;
         expect(findSubentry).to.be.true;
         expect(findOtherEntry).to.equal(null);
     });
 
-    it("test invalid zip", async () => {
+    it('test invalid zip', async () => {
         const rootFolder = process.cwd();
-        const zipExamplePath = path.resolve(rootFolder, "tests/achiver");
+        const zipExamplePath = path.resolve(rootFolder, 'tests/achiver');
         let error = null;
         try {
-            await zip([path.resolve(zipExamplePath, "demo3.entry")], zipPath, zipExamplePath);
+            await zip([path.resolve(zipExamplePath, 'demo3.entry')], zipPath, zipExamplePath);
         } catch (e) {
             error = e;
         }
